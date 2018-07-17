b67cb4f5ee6d058a6c1bd17aef7ccfe2018bcffe
Adrian Pavelescu <adrian@acpgr.com>
2018-07-16 22:22:41 -0400

    add collection list/detail shells

--- src/components/Landing/RegistrationSimple/RegistrationSimple.js
+++ src/components/Landing/RegistrationSimple/RegistrationSimple.js
@@ -1,21 +1,19 @@
-export default {
-  name: 'registration-simple',
-  components: {},
-  props: [],
-  data () {
-    return {
+import { Component, Vue } from 'vue-property-decorator';
+import http from '@/services/http';
+import environment from '@/services/environment';
 
-    };
-  },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-    registerNewUser: function () {
-      console.log('Register stuff');
-    }
-  }
+const api = {
+  register: () => environment.getEndpoint(`account/register`)
 };
+
+@Component
+export default class RegistrationSimple extends Vue {
+  newUser = { username: '', email: '', password: '', firstName: '', lastName: '' };
+
+  registerNewUser () {
+    http.post(api.register(), this.newUser)
+      .then((response) => {
+        console.dir(response);
+      });
+  }
+}
